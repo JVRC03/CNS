c3766339b4bca91986dd56d290cde8bb76d9ab35
~JVRC <140805892+JVRC03@users.noreply.github.com>
2026-01-05 10:03:08 +0530

    Create Task_2.c

---
+++ Task_2.c
@@ -0,0 +1,38 @@
+
+/*   Write a C program that contains a string (char pointer) with a value World
+The program should AND or and XOR each character in this string with 127
+and display the result
+
+*/
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    char *str = malloc(50 * sizeof(char )); 
+    
+    printf("\nEnter a String : ");
+    scanf("%s", str);
+
+    printf("Result after XOR operation : \n");
+    for(char *i = str; *i != '\0'; i++)
+    {
+        printf("%c", *i ^ 127);
+    }
+    
+    printf("\nResult after AND operation: \n");
+    for(char *i = str; *i != '\0'; i++)
+    {
+        printf("%c", *i & 127);
+    }
+    
+    printf("\nResult after OR operation: \n");
+    for(char *i = str; *i != '\0'; i++)
+    {
+        printf("%c", *i | 127);
+    }
+    
+    
+    
+    return 0;
+}
